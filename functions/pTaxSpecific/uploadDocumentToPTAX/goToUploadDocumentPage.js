12f2cf5c79e583e089634dd9b18213c4d6f8ebfd
Frank Edwards <frank.edwards@ptaxresources.com>
2022-02-07 17:05:34 -0800

    Restructured file directory

---
+++ functions/pTaxSpecific/uploadDocumentToPTAX/goToUploadDocumentPage.js
@@ -0,0 +1,19 @@
+const goToUploadDocumentPage = async (driver) => {
+  await driver.switchTo ().defaultContent ();
+
+  //Click on document element in navbar
+  const documentTabOfNavbar = await driver.findElement (
+    By.xpath ('/html/body/form/div[4]/div/ul/li[4]/a/span')
+  );
+  await documentTabOfNavbar.click ();
+
+  //wait until the upload document dropdown is interactable, then click
+  await driver.sleep (5000);
+  const newDocumentElement = await driver.findElement (
+    By.xpath ('/html/body/form/div[4]/div/ul/li[4]/div/ul/li[9]/a/span')
+  );
+  driver.wait (until.elementIsEnabled (newDocumentElement));
+  await newDocumentElement.click ();
+};
+
+module.exports = {goToUploadDocumentPage};
---
+++ functions/pTaxSpecific/uploadDocumentToPTAX/reserveNewDocument.js
@@ -0,0 +1,8 @@
+const reserveNewDocument = async (driver, iframe) => {
+    await driver.switchTo ().frame (iframe);
+    const reserveButton = await driver.findElement (By.id ('UploadBtn'));
+    await driver.wait (until.elementIsEnabled (reserveButton));
+    await reserveButton.click ();
+};
+
+module.exports = {reserveNewDocument};
