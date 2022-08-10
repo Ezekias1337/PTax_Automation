f6a6e00015d62538b773e2f180f19e78c436d72f
Frank Edwards <frankedwards50@gmail.com>
2022-08-10 10:56:23 -0700

    Riverside Assessments Bug Fixes

--- allAutomations/assessmentNotices/states/california/cities/riverside/helpers/navigateToAssessmentData.js
+++ allAutomations/assessmentNotices/states/california/cities/riverside/helpers/navigateToAssessmentData.js
@@ -1,28 +1,46 @@
 const awaitElementLocatedAndReturn = require("../../../../../../../functions/general/awaitElementLocatedAndReturn");
 const scrollElementIntoView = require("../../../../../../../functions/general/scrollElementIntoView");
+const fluentWait = require("../../../../../../../functions/general/fluentWait");
+const { By } = require("selenium-webdriver");
 
 const navigateToAssessmentData = async (driver, assessmentWebsiteSelectors) => {
-  const viewPropertyButton = await awaitElementLocatedAndReturn(
-    driver,
-    assessmentWebsiteSelectors.viewProperty,
-    "xpath"
-  );
-  await viewPropertyButton.click();
+  let navigationSuccessful;
+  try {
+    const viewPropertyButton = await awaitElementLocatedAndReturn(
+      driver,
+      assessmentWebsiteSelectors.viewProperty,
+      "xpath"
+    );
+    await viewPropertyButton.click();
 
-  const viewValueHistoryButton = await awaitElementLocatedAndReturn(
-    driver,
-    assessmentWebsiteSelectors.valueHistory,
-    "xpath"
-  );
-  await viewValueHistoryButton.click();
+    const viewValueHistoryButton = await awaitElementLocatedAndReturn(
+      driver,
+      assessmentWebsiteSelectors.valueHistory,
+      "xpath"
+    );
+    await viewValueHistoryButton.click();
 
-  const totalRollValueElement = await awaitElementLocatedAndReturn(
-    driver,
-    assessmentWebsiteSelectors.viewTotalRollValue,
-    "xpath"
-  );
-  await scrollElementIntoView(driver, totalRollValueElement);
-  await totalRollValueElement.click();
+    await fluentWait(
+      driver,
+      assessmentWebsiteSelectors.viewTotalRollValue,
+      "xpath",
+      15,
+      2,
+      "Failed to find total roll value button. Property has 0 value."
+    );
+
+    const totalRollValueElement = await driver.findElement(
+      By.xpath(assessmentWebsiteSelectors.viewTotalRollValue)
+    );
+    await scrollElementIntoView(driver, totalRollValueElement);
+    await totalRollValueElement.click();
+
+    navigationSuccessful = true;
+  } catch (error) {
+    navigationSuccessful = false;
+  }
+
+  return navigationSuccessful;
 };
 
 module.exports = navigateToAssessmentData;
--- functions/pTaxSpecific/clickNavbar/clickNavbarMenu.js
+++ functions/pTaxSpecific/clickNavbar/clickNavbarMenu.js
@@ -9,36 +9,40 @@ const reportsNavbarClick = require("./individual/reportsNavbarClick");
 const viewNavbarClick = require("./individual/viewNavbarClick");
 
 const clickNavbarMenu = async (driver, section, subOption) => {
-  switch (section) {
-    case "file":
-      await fileNavbarClick(driver, subOption);
-      break;
-    case "edit":
-      await editNavbarClick(driver, subOption);
-      break;
-    case "view":
-      await viewNavbarClick(driver, subOption);
-      break;
-    case "documents":
-      await documentsNavbarClick(driver, subOption);
-      break;
-    case "appeals":
-      await appealsNavbarClick(driver, subOption);
-      break;
-    case "payments":
-      await paymentsNavbarClick(driver, subOption);
-      break;
-    case "reports":
-      await reportsNavbarClick(driver, subOption);
-      break;
-    case "links":
-      await linksNavbarClick(driver, subOption);
-      break;
-    case "help":
-      await helpNavbarClick(driver, subOption);
-      break;
-    default:
-      console.log("No match found for navbar section");
+  try {
+    switch (section) {
+      case "file":
+        await fileNavbarClick(driver, subOption);
+        break;
+      case "edit":
+        await editNavbarClick(driver, subOption);
+        break;
+      case "view":
+        await viewNavbarClick(driver, subOption);
+        break;
+      case "documents":
+        await documentsNavbarClick(driver, subOption);
+        break;
+      case "appeals":
+        await appealsNavbarClick(driver, subOption);
+        break;
+      case "payments":
+        await paymentsNavbarClick(driver, subOption);
+        break;
+      case "reports":
+        await reportsNavbarClick(driver, subOption);
+        break;
+      case "links":
+        await linksNavbarClick(driver, subOption);
+        break;
+      case "help":
+        await helpNavbarClick(driver, subOption);
+        break;
+      default:
+        console.log("No match found for navbar section");
+    }
+  } catch (error) {
+    await clickNavbarMenu(driver, section, subOption);
   }
 };
 
